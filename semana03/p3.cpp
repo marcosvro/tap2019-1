03ac2a08cd88daa5a0b78aec8f345b1c84fdc930
marcos <marcos.v.rodrigues@hotmail.com>
2019-04-05 14:08:21 -0300

    dasda

--- semana03/p3.cpp
+++ semana03/p3.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+vector <int> dist_jovem;
+vector <int> dist_adulto;
+
 // A utility function to find the vertex with minimum distance value, from 
 // the set of vertices not yet included in shortest path tree 
 int minDistance(int dist[], bool sptSet[], int len)
@@ -28,46 +31,51 @@ int printSolution(int dist[], int n)
 
 // Function that implements Dijkstra's single source shortest path algorithm 
 // for a graph represented using adjacency matrix representation 
-int* dijkstra(vector< vector<int> > graph, int src, int len, vector< vector<int> > idade, int op)
+void dijkstra(vector< vector<int> > graph, int src, int len, vector< vector<int> > idade, int op)
 {
-     int *dist = (int*) malloc(sizeof(int)*(len-1));     // The output array.  dist[i] will hold the shortest 
+
+    int dist[len];     // The output array.  dist[i] will hold the shortest 
                       // distance from src to i 
-   
-     bool sptSet[len]; // sptSet[i] will be true if vertex i is included in shortest 
+    bool sptSet[len]; // sptSet[i] will be true if vertex i is included in shortest 
                      // path tree or shortest distance from src to i is finalized 
    
      // Initialize all distances as INFINITE and stpSet[] as false 
-     for (int i = 0; i < len; i++) 
+    for (int i = 0; i < len; i++) 
         dist[i] = INT_MAX, sptSet[i] = false; 
    
-     // Distance of source vertex from itself is always 0 
-     dist[src] = 0; 
+    // Distance of source vertex from itself is always 0 
+    dist[src] = 0; 
    
-     // Find shortest path for all vertices 
-     for (int count = 0; count < len-1; count++)
-     { 
-       // Pick the minimum distance vertex from the set of vertices not 
-       // yet processed. u is always equal to src in the first iteration. 
-       int u = minDistance(dist, sptSet, len); 
+    // Find shortest path for all vertices 
+    for (int count = 0; count < len-1; count++)
+    { 
+        // Pick the minimum distance vertex from the set of vertices not 
+        // yet processed. u is always equal to src in the first iteration. 
+        int u = minDistance(dist, sptSet, len); 
    
        // Mark the picked vertex as processed 
-       sptSet[u] = true; 
+        sptSet[u] = true; 
 
-       // Update dist value of the adjacent vertices of the picked vertex. 
-       for (int v = 0; v < len; v++) 
+        // Update dist value of the adjacent vertices of the picked vertex. 
+        for (int v = 0; v < len; v++)
+        // Update dist[v] only if is not in sptSet, there is an edge from  
+        // u to v, and total weight of path from src to  v through u is  
+        // smaller than current value of dist[v] 
+            if (!sptSet[v] && graph[u][v] && idade[u][v] == op && dist[u] != INT_MAX && dist[u]+graph[u][v] < dist[v]) 
+                dist[v] = dist[u] + graph[u][v]; 
+    } 
    
-         // Update dist[v] only if is not in sptSet, there is an edge from  
-         // u to v, and total weight of path from src to  v through u is  
-         // smaller than current value of dist[v] 
-         if (!sptSet[v] && graph[u][v] && idade[u][v] == op && dist[u] != INT_MAX  
-                                       && dist[u]+graph[u][v] < dist[v]) 
-            dist[v] = dist[u] + graph[u][v]; 
-     } 
-   
-     // print the constructed distance array 
-     //printSolution(dist, len);
-     return dist;
-     
+    // print the constructed distance array 
+    //printSolution(dist, len);
+    if (op == -1) {
+        dist_jovem.resize(len);
+        for (int i=0; i<len; i++)
+            dist_jovem[i] = dist[i];
+    } else {
+        dist_adulto.resize(len);
+        for (int i=0; i<len; i++)
+            dist_adulto[i] = dist[i];
+    }
 }
 
 //function for printing the elements in a list 
@@ -170,8 +178,8 @@ int main() {
             id_adulto_pos = tradutor[ADULTO_POS]-1;
         }
 
-        int *dist_jovem = dijkstra(m_adjacente, id_jovem_pos, len-1, m_idade, -1);
-        int *dist_adulto = dijkstra(m_adjacente, id_adulto_pos, len-1, m_idade, 1);
+        dijkstra(m_adjacente, id_jovem_pos, len-1, m_idade, -1);
+        dijkstra(m_adjacente, id_adulto_pos, len-1, m_idade, 1);
 
         int ind_menor_lugar;
         int menor_custo = INT_MAX;
@@ -208,9 +216,6 @@ int main() {
         cout << menor_custo << " ";
         showlist(lista_final);
         cout << endl;
-
-        free(dist_jovem);
-        free(dist_adulto);
     }
     return 0;
 }
